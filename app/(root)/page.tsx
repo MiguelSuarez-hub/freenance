5714cb302ccd79490fd0556ebe926eacd2e563a5
MiguelSuarez-hub <miguel.suarezf05@gmail.com>
2024-08-31 10:49:44 -0500

    Created Right sidebar, with credit card views

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,35 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 export default function Home() {
-  const loggedIn = { firstName: 'Miguel'}
+  const loggedIn = {
+    firstName: "Miguel",
+    lastName: "Suarez",
+    email: "migue@gmail.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-          <HeaderBox 
-            type='greeting'
-            title='Welcome'
-            user={loggedIn?.firstName || 'Guest'}
-            subtext='Access and manage your finances'
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your finances"
           />
-          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1300000} />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1300000}
+          />
         </header>
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 125000 }, { currentBalance: 1175000 }]}
+      />
     </section>
   );
 }
